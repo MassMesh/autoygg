94cb6611aec16ce5e12fed2750b71224b6828efd
Ward Vandewege <ward@jhvc.com>
2020-07-19 10:50:08 -0400

    Add more tests.

--- internal/client_test.go
+++ internal/client_test.go
@@ -18,10 +18,13 @@ func Test(t *testing.T) {
 	check.TestingT(t)
 }
 
+const GatewayPort = "61234"
+
 var _ = check.Suite(&Suite{})
 
 type Suite struct{}
 
+var YggAddress string
 var serverConfigDir string
 var srv *http.Server
 
@@ -37,7 +40,6 @@ func StartServer(c *check.C) {
 	serverLoadConfig(serverConfigDir)
 
 	db := setupDB("sqlite3", viper.GetString("StateDir")+"/autoygg.db")
-	defer db.Close()
 	r := setupRouter(db)
 
 	srv = &http.Server{
@@ -46,6 +48,7 @@ func StartServer(c *check.C) {
 	}
 
 	go func() {
+		defer db.Close()
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			c.Fatal(err)
 		}
@@ -59,12 +62,24 @@ func (s *Suite) SetUpSuite(c *check.C) {
 		c.Fatal(err)
 	}
 
+	// Get our own Yggdrasil Address
+	YggAddress, err = getSelfAddress()
+	c.Assert(err, check.Equals, nil)
+
 	// Populate a custom config file
-	serverYaml := []byte("---\nListenHost: \"::1\"\nListenPort: \"61234\"\nStateDir: \"" + serverConfigDir + "\"\n")
+	serverYaml := []byte("---\nListenHost: \"" + YggAddress + "\"\nListenPort: \"" + GatewayPort + "\"\nStateDir: \"" + serverConfigDir + "\"\n")
 	configFile := filepath.Join(serverConfigDir, "server.yaml")
 	err = ioutil.WriteFile(configFile, serverYaml, 0644)
 	if err != nil {
 		c.Fatalf("Couldn't write file %s", configFile)
+	}
+
+	// And an empty accessList file
+	accessList := []byte("AccessList:\n")
+	accessListFile := filepath.Join(serverConfigDir, "accesslist.yaml")
+	err = ioutil.WriteFile(accessListFile, accessList, 0644)
+	if err != nil {
+		c.Fatalf("Couldn't write file %s", accessListFile)
 	}
 
 	StartServer(c)
@@ -111,11 +126,11 @@ func (*Suite) TestConfigLoading(c *check.C) {
 	c.Assert(viper.GetString("gatewayPort"), check.Equals, "9090")
 }
 
-func (*Suite) TestInfoCall(c *check.C) {
+func (*Suite) TestInfo(c *check.C) {
 	// Load default config
 	fs := clientCreateFlagSet()
 
-	i, err := doInfoRequest(fs, "::1", "61234")
+	i, err := doInfoRequest(fs, YggAddress, GatewayPort)
 
 	c.Assert(err, check.Equals, nil)
 	c.Check(i.GatewayOwner, check.Equals, "Some One <someone@example.com>")
@@ -127,4 +142,72 @@ func (*Suite) TestInfoCall(c *check.C) {
 	c.Check(i.RequireRegistration, check.Equals, true)
 	c.Check(i.RequireApproval, check.Equals, true)
 	c.Check(i.AccessListEnabled, check.Equals, true)
+}
+
+func CustomClientConfig(c *check.C) (tmpDir string) {
+	tmpDir, err := ioutil.TempDir("", "autoygg-client-test")
+	if err != nil {
+		c.Fatal(err)
+	}
+
+	// Populate a custom config file
+	clientYaml := []byte("---\nGatewayHost: \"" + YggAddress + "\"\nGatewayPort: \"" + GatewayPort + "\"\nStateDir: \"" + tmpDir + "\"")
+	configFile := filepath.Join(tmpDir, "client.yaml")
+	err = ioutil.WriteFile(configFile, clientYaml, 0644)
+	if err != nil {
+		c.Fatalf("Couldn't write file %s", configFile)
+	}
+	return
+}
+
+func (*Suite) TestRegistration(c *check.C) {
+	// Load default config
+	fs := clientCreateFlagSet()
+
+	tmpDir := CustomClientConfig(c)
+	defer os.RemoveAll(tmpDir)
+
+	// Load custom config file and make sure the values are read back correctly
+	clientLoadConfig(tmpDir)
+
+	var err error
+	var State state
+	State, err = loadState(State)
+	c.Assert(err, check.Equals, nil)
+
+	// Try to register when our address is not on the accesslist
+	r, State, err := doRequest(fs, "register", YggAddress, GatewayPort, State)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(r.Error, check.Equals, "Registration not allowed")
+
+	// Changing the access list file will trigger an automatic config reload in the server
+	accessList := []byte("AccessList:\n- " + YggAddress + "\n")
+	accessListFile := filepath.Join(serverConfigDir, "accesslist.yaml")
+	err = ioutil.WriteFile(accessListFile, accessList, 0644)
+	if err != nil {
+		c.Fatalf("Couldn't write file %s", accessListFile)
+	}
+
+	r, State, err = doRequest(fs, "register", YggAddress, GatewayPort, State)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(r.Error, check.Equals, "")
+
+	r, State, err = doRequest(fs, "renew", YggAddress, GatewayPort, State)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(r.Error, check.Equals, "")
+
+	r, State, err = doRequest(fs, "release", YggAddress, GatewayPort, State)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(r.Error, check.Equals, "")
+
+	// Release non-existent lease
+	r, State, err = doRequest(fs, "release", YggAddress, GatewayPort, State)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(r.Error, check.Equals, "Registration not found")
+
+	// Renew non-existent lease
+	r, State, err = doRequest(fs, "renew", YggAddress, GatewayPort, State)
+	c.Assert(err, check.Equals, nil)
+	c.Assert(r.Error, check.Equals, "Registration not found")
+
 }
